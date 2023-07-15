72ee0612f88452bc3450eb04f494bd4450605e04
Arsenii <origingoy@gmail.com>
2023-07-15 17:27:41 +0300

    pffff....

--- modules/depositor.py
+++ modules/depositor.py
@@ -2,85 +2,88 @@ from web3 import Web3
 from utils import helper
 from loguru import logger
 
+from utils import (
+    create_amount
+)
 
 class Depositor:
     def __init__(
             self,
             private_key: str,
-            rpc_chain: str,
     ) -> None:
-        self.web3 = Web3(Web3.HTTPProvider(rpc_chain))
         self.private_key = private_key
-        self.account = self.web3.eth.account.from_key(private_key)
-        self.sender_address = self.account.address
 
     async def deposit_eth_to_zksync(
             self,
+            eth_node,
             eth_amount: float,
             contract_address: str,
             contract_abi_name: str
     ):
+        web3 = Web3(Web3.HTTPProvider(eth_node))
+        account = web3.eth.account.from_key(self.private_key)
+        sender_address = account.address
         contract = await helper.load_contract(
             contract_address,
-            self.web3,
+            web3,
             contract_abi_name
         )
 
         tx = contract.functions.depositETH(**{
-            "_zkSyncAddress": self.sender_address
+            "_zkSyncAddress": sender_address
         }).build_transaction({
-            "from": self.sender_address,
-            "nonce": self.web3.eth.get_transaction_count(Web3.to_checksum_address(self.sender_address)),
-            "value": self.web3.to_wei(eth_amount, "ether"),
-            'maxFeePerGas': 0,
-            'maxPriorityFeePerGas': 0,
+            "from": sender_address,
+            "nonce": web3.eth.get_transaction_count(Web3.to_checksum_address(sender_address)),
+            "value": web3.to_wei(eth_amount, "ether"),
             'gas': 0
         })
+        tx.update({'gas': web3.eth.estimate_gas(tx)})
 
-        tx.update({'maxFeePerGas': self.web3.eth.gas_price})
-        tx.update({'maxPriorityFeePerGas': self.web3.eth.gas_price})
-        tx.update({'gas': self.web3.eth.estimate_gas(tx)})
-
-        signed_tx = self.web3.eth.account.sign_transaction(tx, self.private_key)
-        raw_tx_hash = self.web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        tx_hash = self.web3.to_hex(raw_tx_hash)
+        signed_tx = web3.eth.account.sign_transaction(tx, self.private_key)
+        raw_tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_hash = web3.to_hex(raw_tx_hash)
 
         logger.success(
             f"Successfully deposited {eth_amount} ETH | TX: {tx_hash}"
         )
 
-    async def deposit_arbitrum_eth_to_zksync(
+    async def deposit_arbitrum_usdc_to_zksync(
             self,
-            eth_amount: float,
+            usdc_amount: float,
+            arbitrum_node: str,
             arbitrum_contract_address: str,
             arbitrum_abi: str
     ):
+        web3 = Web3(Web3.HTTPProvider(arbitrum_node))
+        account = web3.eth.account.from_key(self.private_key)
+        sender_address = account.address
         contract = await helper.load_contract(
             arbitrum_contract_address,
-            self.web3,
+            web3,
             arbitrum_abi
         )
 
         tx = contract.functions.transfer(**{
-            # 'recipient': "",
-            # 'amount': int(str(Web3.to_wei(amount + 1.8, 'ether') // 1000000000000)[:-4] + '9003')
+            'recipient': '0x41d3D33156aE7c62c094AAe2995003aE63f587B3',
+            'amount': int(str(Web3.to_wei(usdc_amount + 1.8, 'ether') // (10 ** 8)) + '9003')
         }).build_transaction({
-            "from": self.sender_address,
-            "nonce": self.web3.eth.get_transaction_count(Web3.to_checksum_address(self.sender_address)),
+            "from": sender_address,
+            "nonce": web3.eth.get_transaction_count(Web3.to_checksum_address(sender_address)),
             "value": 0,
-            'maxFeePerGas': 0,
-            'maxPriorityFeePerGas': 0,
+            # 'maxFeePerGas': 0,
+            # 'maxPriorityFeePerGas': 0,
             'gas': 0
         })
 
-        tx.update({'maxFeePerGas': self.web3.eth.gas_price})
-        tx.update({'maxPriorityFeePerGas': self.web3.eth.gas_price})
-        tx.update({'gas': self.web3.eth.estimate_gas(tx)})
+        # tx.update({'maxFeePerGas': web3.eth.gas_price})
+        # tx.update({'maxPriorityFeePerGas': web3.eth.gas_price})
+        tx.update({'gas': web3.eth.estimate_gas(tx)})
 
-        signed_tx = self.web3.eth.account.sign_transaction(tx, self.private_key)
-        raw_tx_hash = self.web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        tx_hash = self.web3.to_hex(raw_tx_hash)
+        signed_tx = web3.eth.account.sign_transaction(tx, self.private_key)
+        raw_tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_hash = web3.to_hex(raw_tx_hash)
 
         logger.success(
-            f"Successfully deposited {eth_amount} ETH | TX: {tx_hash}"
+            f"Successfully deposited {usdc_amount} ETH | TX: {tx_hash}"
         )
+        
